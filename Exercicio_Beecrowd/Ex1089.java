423a8de865a1cabde29ca38cae74e125a2d8b5c7
Joao Pedro Sehnem dos Santos <151693736+joaosehn2406@users.noreply.github.com>
2024-04-16 07:54:10 -0300

    Add files via upload

---
+++ Exercicio_Beecrowd/Ex1089.java
@@ -0,0 +1,27 @@
+package Exercicios_Beecrowd;
+
+import java.util.Scanner;
+
+public class Ex1089 {
+        
+    public Ex1089(){
+
+    Scanner in = new Scanner(System.in);
+        
+    int N = in.nextInt();
+    int contador = 0;
+    for(int i = 0; i < N; i++) {
+
+        
+
+    }
+    
+
+
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
